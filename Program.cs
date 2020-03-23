a657b7347b8d9669af635d8ac9c29d32f917b480
C#Developer <shahzodkiller11@gmail.com>
2020-03-23 21:54:48 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            // double a=19.8;
+            // double b=99.11;
+            // Console.WriteLine(Math.Sqrt(a*b));
+
+            //2
+            // double a=1.4;
+            // double b=-5.5;
+            // double c=0.6;
+            // double ac=Math.Abs(a-c);
+            // double bc=Math.Abs(b-c);  
+            // System.Console.WriteLine($"ac:{ac}");
+            // System.Console.WriteLine($"bc:{bc}");
+
+            //3
+            // double x1=-6.2;
+            // double y1=5.2;
+            // double x2=2.1;
+            // double y2=9.8;
+            // System.Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)),2));
+
+            //4
+            // int a=41;
+            // string word=a.ToString();
+            // Console.Write(word[1]);
+            // Console.Write(word[0]);
+
+            //5
+            // int N=10985;
+            // int hours=N/60;
+            // System.Console.WriteLine($"hours:{hours}");
+
+            //6 
+            // int  e = 202, i = e % 7;
+            // Console.WriteLine(i);
+        }
+    }
+}
